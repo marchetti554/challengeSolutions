b7220949df027ef36ed1bfbf8c99e627b8c90ab8
marchetti554 <marchetti554@gmail.com>
2023-04-20 11:00:47 -0300

    Added Java solution to groupAnagrams

--- leetCode/groupAnagrams/java/GroupAnagrams.java
+++ leetCode/groupAnagrams/java/GroupAnagrams.java
@@ -1,8 +1,25 @@
 public class GroupAnagrams {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> input = List.of("eat", "tea", "tan", "ate", "nat", "bat");
+
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String s : input) {
+            String sortedString = sortString(s);
+            if (anagramMap.containsKey(sortedString)) {
+                anagramMap.get(sortedString).add(s);
+            } else {
+                anagramMap.put(sortedString, new ArrayList<>(List.of(s)));
+            }
+        }
+
+        System.out.println(anagramMap.values());
     }
 
-
+    private static String sortString(String s) {
+        char[] stringChars = s.toCharArray();
+        Arrays.sort(stringChars);
+        return String.valueOf(stringChars);
+    }
 }

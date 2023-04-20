1197e738503149a6417369af6784b9d7958cb387
marchetti554 <marchetti554@gmail.com>
2023-04-20 11:34:16 -0300

    Added Rust solution

--- leetCode/groupAnagrams/rust/group_anagrams.rs
+++ leetCode/groupAnagrams/rust/group_anagrams.rs
@@ -1,8 +1,20 @@
-pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+use std::collections::HashMap;
 
+fn main() {
+    group_anagrams(vec!["eat".to_string(), "tea".to_string(), "tan".to_string(), "ate".to_string(), "nat".to_string(), "bat".to_string()]);
 }
 
-fn test() {
-    let array: [String; 32] = Default:default();
-    group_anagrams(array)
+// Working but need more refinement and understanding
+fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut h = HashMap::new();
+
+    for s in strs {
+        let mut key: Vec<char> = s.chars().collect();
+        key.sort();
+        h.entry(key).or_insert(vec![]).push(s);
+    }
+
+    println!("{:?}", h.values().cloned().collect::<Vec<_>>());
+
+    return vec![vec!["xd".to_string()]];
 }
